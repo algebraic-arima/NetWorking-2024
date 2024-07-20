d7e9b30c7ae32193baa333dcce9164dfedfe90f3
algebraic-arima <lfk.alg@gmail.com>
2024-07-20 21:45:37 +0800

    ffin(?)

---
+++ socks5-udp/gg.go
@@ -0,0 +1,38 @@
+package main
+
+var adore = map[string][]string{
+	"Chanson of Justice and Impartiality": {
+		"Le Souvenir avec le crepuscule",
+		"Ballad of Many Waters",
+		"Leisurely Days in Fontaine",
+		"Poesy of Chrysolite",
+		"Pluie sur la ville",
+		"Romaritime Recollection",
+		"Pilot's Rest",
+		"Limpide est le sanglot d'eau",
+		"Clair de lune",
+		"Where all Waters Converge",
+		"Raven Gloss of Darkness",
+		"Le spectacle doit continuer",
+	},
+	"Les grandes eaux qu'elle avait veillées": {
+		"Mellow Alize",
+		"Songe de la plus haute tour",
+		"Le commencement de la fin",
+		"Dream Anamnesis",
+	},
+	"Galliard of Brass and Iron": {
+		"Clockwork Waltz",
+		"The Rotating Realm",
+		"Welcome to the Industry",
+		"Le duc sous l'eau",
+		"The Faded Idyll",
+		"The Drifting Beauty",
+		"Huldra's Retreat",
+		"Whispering Dewdrops",
+	},
+	"Versi di Petrichor": {
+		"Regali Teneri",
+		"Ed e subito sera",
+	},
+}
